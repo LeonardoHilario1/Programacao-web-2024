879b2752ae3aa8f1147edd5e734d8d63b2ddf81e
LeonardoHilario1 <134790968+LeonardoHilario1@users.noreply.github.com>
2024-03-22 10:29:10 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,19 @@
+const a=2;
+const b=3;
+const d="2";
+const e="3";
+
+let c= a+b;
+console.log('O Resultado da soma entre ' + a +' e ' + b +' é '+ c);
+
+c = a -b;
+console.log('O Resultado da subtração entre ' + a +' e ' + b +' é '+ c);
+
+c= a* b;
+console.log('O Resultado da multiplicação entre ' + a +' e ' + b +' é '+ c);
+
+c =a/b;
+console.log('O Resultado da Divisão entre ' + a +' e ' + b +' é '+ c);
+
+console.log("O resultado da soma entre "+ d +" e " + e + " é "+ d + e);
+
